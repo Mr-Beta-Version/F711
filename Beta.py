909aef81a10c3b05ba747202641d06707ccc9f38
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-05-20 14:27:29 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -5,4 +5,5 @@ if bit == '64bit':
     from F711 import bnsbuy
     bnsbuy()
 elif bit == '32bit':
-    exit('\x1b[1;91m\n\tOpps Your Device Not Supported')
+    from F32 import bnsbuy
+    bnsbuy()
