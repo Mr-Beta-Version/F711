806d8402e8888833e3eacd1d629bd5d6fffc2aac
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-05-30 11:02:54 +0600

    Delete Beta.py

--- Beta.py
+++
@@ -1,9 +0,0 @@
-import os, platform
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from F711 import bnsbuy
-    bnsbuy()
-elif bit == '32bit':
-    from F32 import bnsbuy
-    bnsbuy()
